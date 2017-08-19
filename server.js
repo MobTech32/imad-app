5e92d0243afa940e322184f620fb55d56c470b14
MobTech32 <30855936+MobTech32@users.noreply.github.com>
2017-08-19 23:52:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -210,7 +210,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
     res.status(403).send('Only logged in users can comment');
     }
 });
-/*
+
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
@@ -230,7 +230,7 @@ app.get('/articles/:articleName', function (req, res) {
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
-*/
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
