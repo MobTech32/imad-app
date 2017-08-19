bba2130ac3001259a766cc32296a8ad71bbd12c1
MobTech32 <30855936+MobTech32@users.noreply.github.com>
2017-08-19 23:33:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -207,14 +207,14 @@ app.post('/submit-comment/:articleName', function (req, res) {
             }
        });     
     } else {
-        res.status(403).send('Only logged in users can comment');
+    res.status(403).send('Only logged in users can comment');
     }
 });
 /*
 app.get('/articles/:articleName', function (req, res) {
   // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
   pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-    if (err) {
+   if (err) {
         res.status(500).send(err.toString());
     } else {
         if (result.rows.length === 0) {
@@ -230,10 +230,9 @@ app.get('/articles/:articleName', function (req, res) {
 app.get('/ui/:fileName', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
-
+*/
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
-*/
