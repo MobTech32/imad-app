99da279cf5f6c33b25964526a416d7920b1fe0b2
MobTech32 <30855936+MobTech32@users.noreply.github.com>
2017-08-20 18:06:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -226,7 +226,7 @@ app.get('/articles/:articleName', function (req, res) {
   });
 });
 
-app.get('/ui/index.html', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
